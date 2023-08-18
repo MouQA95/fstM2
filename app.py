de11b4063c0e03800c1d71b038429671afd2f6da
MouQA95 <117269287+MouQA95@users.noreply.github.com>
2023-08-18 09:50:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class MyHandler(BaseHTTPRequestHandler):
   \____\_______/
 
 
-Good Evening from Manoj! Today is 3rd July 
+Good Evening from Mounami! Today is 18th August 2023
 ''')
 
 def run():
